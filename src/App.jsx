0f6339c3933dbed409bda879158831aea03c0e59
rifat3568 <mazharulislam3569@gmail.com>
2024-02-19 15:02:58 +0600

    added

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import MainLayout from './LayOut/MainLayout'
+
+function App() {
+  
+
+  return (
+    <div className='w-11/12 mx-auto'>
+     <MainLayout></MainLayout>
+    </div>
+  )
+}
+
+export default App
